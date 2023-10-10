1c81057fae231d1210a9ff9ca660977a55b76e64
rabiaaminu <ahmadrabia929@gmail.com>
2023-10-10 13:34:43 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,20 +2,19 @@
 /**
  * main - Entry point
  *
- * Return: Alway 0 (Sucess)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	_putchar('_')
-	_putchar('p')
-	_putchar('u')
-	_putchar('t')
-	_putchar('c')
-	_putchar('h')
-	_putchar('a')
-	_putchar('r')
-	_putchar('/n')
-	return (0);
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
 }
 
