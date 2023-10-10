22d9d0974e1ad20d1fd060eded7ffce702e9eaee
rabiaaminu <ahmadrabia929@gmail.com>
2023-10-10 15:19:59 -0700

    program that finds and prints the first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,45 +1,40 @@
 #include <stdio.h>
 /**
- * print_fib - prints the first 50 Fibonacci numbers,
- * starting with 1 and 2, followed by a new line.
- * @len: the length of with the fib sequance should print to.
+ * main - Entry point
  *
- * Return: void
- */
-void print_fib(int len)
-{
-	unsigned int target, i;
-
-	unsigned int former_l, former_r,;
-	unsigned int latter_l, latter_r;
-	unsigned int fib_l, fib_r;
-
-	f = 1;
-	l = 2;
-	target = len - 2;
-	for (i = 0 ; i < target ; i++)
-	{
-		fib = f + l;
-		printf("%d : ", i + 4);
-		printf("%.0Lf" , fib);
-		f = l;
-		l = fib;
-		if (i < target - 1)
-			printf("\n");
-	}
-	putchar('\n');
-}
-
-
-/**
- * main - Entry point,
- *
- *
- * Return: Always 0 (sucess)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	print_fib(98);
-	return (0);
+unsigned int a = 1, b = 2, c;
+int count = 2;
+putchar('1');
+putchar(',');
+putchar(' ');
+putchar('2');
+while (count < 98)
+{
+c = a + b;
+if (count < 97)
+{
+putchar(',');
+putchar(' ');
+}
+count++;
+putchar(' ');
+if (c > 9)
+{
+putchar((c / 10) + '0');
+putchar((c % 10) + '0');
+}
+else
+{
+putchar(c + '0');
+}
+a = b;
+b = c;
+}
+putchar('\n');
+return (0);
 }
 
