6c94ecb94946486eadb204089f05ef38aa1709b9
rabiaaminu <ahmadrabia929@gmail.com>
2023-10-18 12:43:42 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,10 +8,7 @@ int main(void)
 
   a[2] = 1024;
   p = &n;
-
-  n = 98;  /* New line of code */
-
-  printf("a[2] = %d\n", a[2]);
+  *(a + 2) = 98, printf("a[2] = %d\n", *(a + 2));
   return (0);
 }
 
