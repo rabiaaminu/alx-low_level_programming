a27946899c2f27fca765ebaad76c62ee5369e114
rabiaaminu <ahmadrabia929@gmail.com>
2023-10-18 12:40:01 -0700

    magic

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int n;
+  int a[5];
+  int *p;
+
+  a[2] = 1024;
+  p = &n;
+
+  n = 98;  /* New line of code */
+
+  printf("a[2] = %d\n", a[2]);
+  return (0);
+}
+
