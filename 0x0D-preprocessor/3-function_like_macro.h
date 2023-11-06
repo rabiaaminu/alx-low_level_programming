a6c55c8c4bf3ed28c40187a77c0273ad642aed4b
rabiaaminu <ahmadrabia929@gmail.com>
2023-11-06 13:08:46 -0800

    abs

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -5,9 +5,10 @@
  * File: 3-function_like_macro.h
  * Auth: Rabia Aminu Ahmad
  * Desc: Header file that defines a function-like macro ABS(x)
- *       that computes the absolute value of a number x.
+ * that computes the absolute value of a number x.
  */
 
-#define ABS(x) ((x) < 0 ? (-x) : (x))
+#define ABS(x) ((x) < 0 ? ((x) * -1) : (x))
 
 #endif
+
