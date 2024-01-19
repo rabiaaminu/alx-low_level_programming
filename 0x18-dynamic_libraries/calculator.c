3118b723806824d924417cdddc9388acd7a18309
rabiaaminu <ahmadrabia929@gmail.com>
2024-01-19 08:45:42 -0800

    100-operations.so

--- 0x18-dynamic_libraries/calculator.c
+++ 0x18-dynamic_libraries/calculator.c
@@ -1,63 +1,25 @@
-#!/usr/bin/python3
-def add(a, b):
-    """My addition function
+int add(int a, int b)
+{
+	return (a + b);
+}
 
-    Args:
-        a: first integer
-        b: second integer
+int sub(int a, int b)
+{
+	return (a - b);
+}
 
-    Returns:The return value. a + b
-    """
-    return (a + b)
+int mul(int a, int b)
+{
+	return (a * b);
+}
 
+int div(int a, int b)
+{
+	return (a / b);
+}
 
-def sub(a, b):
-    """My subtraction function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a - b
-    """
-    return (a - b)
-
-
-def mul(a, b):
-    """My multiplication function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a * b
-    """
-    return (a * b)
-
-
-def div(a, b):
-    """My division function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a / b
-    """
-    return int(a / b)
-
-def mod(a, b):
-    """My modulo function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a / b
-    """
-    return int(a % b)
+int mod(int a, int b)
+{
+	return (a % b);
+}
 
